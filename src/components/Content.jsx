1cb74324fdd5319516e1316295a083ba10aed1fd
Hovhannes <hovhannes@hovhannes-2.local>
2022-09-28 18:21:15 +0400

    uploaded files

---
+++ src/components/Content.jsx
@@ -0,0 +1,9 @@
+function Content() {
+  return (
+    <div className="container-md content">
+      <h1>Добро пожаловать в Армянскую Ассоциацию МГТУ!</h1>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark navbar-custom">
+        <div className="container-md">
+          <a className="navbar-brand" href="#">ААМ</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Главная</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+              <button className="btn btn-outline-light" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
